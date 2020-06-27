c85d4aa32dfffe294acd407668aec9d68ddca146
Josh Leverette <coder543@gmail.com>
2020-06-27 14:43:50 -0400

    add influxdb support

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,23 +1,39 @@
 package monitor
 
 import (
-	"encoding/json"
+	"context"
 	"envmon/sensor"
+	"github.com/influxdata/influxdb-client-go"
 	"log"
 	"time"
 )
 
 func Start() {
+	client := influxdb2.NewClient("http://localhost:8081", "admin:admin")
+	writeApi := client.WriteApiBlocking("", "db0")
 	t := time.NewTicker(5 * time.Second)
 	for {
 		r := sensor.Read()
 		log.Printf("%s", r)
 
-		_, err := json.Marshal(&r)
+		p := influxdb2.NewPoint(
+			"environment",
+			map[string]string{
+				"location": "inside",
+			},
+			map[string]interface{}{
+				"temperature": r.Temperature,
+				"humidity":    r.Humidity,
+				"pressure":    r.Pressure,
+				"altitude":    r.Altitude,
+			},
+			time.Now(),
+		)
+
+		err := writeApi.WritePoint(context.Background(), p)
 		if err != nil {
-			log.Panic(err)
+			log.Println(err)
 		}
-		//log.Printf("json: %s", string(jsonBytes))
 		<-t.C
 	}
 }
